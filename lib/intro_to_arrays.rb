55e939348422039dee85d222e84f962fabfa602f
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-30 23:53:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,12 +10,12 @@ def first_element(my_first_element)
    my_first_element[0]
 end
 
-def third_element(array, index_number)
-  two_cats[2]
+def third_element(my_third_element)
+  my_third_element[2]
 end
 
-def last_element(array, index_number)
-  two_cats[-1]
+def last_element(my_last_element)
+  my_last_element[-1]
 end
 
 def first_element_with_array_methods(array)
