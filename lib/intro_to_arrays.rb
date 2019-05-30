acc883ec8f41c93c06956da784fe923a4935e2d9
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-30 23:54:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,11 +19,11 @@ def last_element(my_last_element)
 end
 
 def first_element_with_array_methods(first_country)
-  two_cats.first
+  first_country.first
 end
 
-def last_element_with_array_methods(array)
-  two_cats.last
+def last_element_with_array_methods(last_country)
+  last_country.last
 end
 
 def length_of_array(array)
