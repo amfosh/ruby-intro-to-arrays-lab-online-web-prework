d6bafa24e8020cf024f177cc6e2dfab7bc266ff8
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-30 23:52:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,7 +3,7 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  my_two_array = ["parker","cookie"]
+  my_two_array = ["parker", "cookie"]
 end
 
 def first_element(array, index_number)
