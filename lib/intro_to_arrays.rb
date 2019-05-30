8e2ab5f23337636e57b5d7f13824e4cc17aba10c
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-30 23:48:20 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  two_cats = ["taylor_swift", "cookie"]
+end
+
+def first_element(array, index_number)
+  two_cats[0]
+end
+
+def third_element(array, index_number)
+  two_cats[2]
+end
+
+def last_element(array, index_number)
+  two_cats[-1]
+end
+
+def first_element_with_array_methods(array)
+  two_cats.first
+end
+
+def last_element_with_array_methods(array)
+  two_cats.last
+end
+
+def length_of_array(array)
+  two_cats.length
+end
+  
